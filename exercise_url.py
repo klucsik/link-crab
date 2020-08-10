b75193fa52e3ba3e77ca7ab07ca2785f6da0234b
klucsik <klucsik.krisztian@gmail.com>
2020-08-10 14:24:44 +0200

    (fix) request exceptions are handled

--- exercise_url.py
+++ exercise_url.py
@@ -6,12 +6,15 @@ def exercise_url(session, url):
     if not check_url_validity(url):
         print(f"invalid link: {url}")
         return False
-    resp = session.get(url)
-    accessible = resp.ok and resp.url == url
-    if not accessible:
-        print(f"    not accessible: statuscode: {resp.status_code}, url: {resp.url}")
-    print(f"[*] link: {url} - {resp.status_code} - {resp.elapsed.total_seconds()*1000} ms - accessible: {accessible}")
-    outcome=[url,resp.status_code, int(resp.elapsed.total_seconds()*1000), accessible]
+    try:
+        resp = session.get(url)
+        accessible = resp.ok and resp.url == url
+        if not accessible:
+            print(f"    not accessible: statuscode: {resp.status_code}, url: {resp.url}")
+        print(f"[*] link: {url} - {resp.status_code} - {resp.elapsed.total_seconds()*1000} ms - accessible: {accessible}")
+        outcome=[url,resp.status_code, int(resp.elapsed.total_seconds()*1000), accessible]
+    except:
+        outcome=[url,err,err,False]
     return outcome
 
 
