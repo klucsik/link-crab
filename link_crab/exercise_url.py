4d7357fb1bc849e42b2a3c96242afa4834fc73d2
tothmate92 <66313640+tothmate92@users.noreply.github.com>
2020-08-16 23:00:48 +0200

    Update exercise_url.py

--- link_crab/exercise_url.py
+++ link_crab/exercise_url.py
@@ -33,7 +33,7 @@ def exercise_url(session, url):
         print(f"[*] {outcome[0]} - {GREEN if outcome[1]==200 else RED} {outcome[1]} {RESET} - {GREEN if outcome[0]==outcome[4] else YELLOW} {outcome[4]} {RESET} - {outcome[2]} ms - accessible: {GREEN if outcome[3]==True else YELLOW} {outcome[3]} {RESET}")
 
     except:
-        outcome=[url,err,err,False, err]
+        outcome=[url, 'err', 'err', False, 'err']
     return outcome
 
 
