97409abf8a05f1e33b9d134ab30a6ff938639a8c
klucsik <klucsik.krisztian@gmail.com>
2020-08-25 03:36:40 +0200

    (fix) test failed becouse of logging change

--- tests/test_gather_links.py
+++ tests/test_gather_links.py
@@ -7,26 +7,29 @@ from link_crab.gather_links import get_all_website_links, crawl, gather_links
 import pytest
 from tests.test_excercise_url import test_session
 from tests.test_app import mock_app
+import logging
 
 def test_url_gathering(test_session):
     links = set()
     assert len(get_all_website_links(test_session,'http://127.0.0.1:5000', links)) == 8
 
-def test_crawl_url_already_visited_not_collected(test_session, capsys):
+def test_crawl_url_already_visited_not_collected(test_session, caplog):
+    caplog.set_level(logging.DEBUG)
     links = set()
     url='http://www.example.com/xmpl'
     links.add(url)
     crawl(test_session,url,links,'www.example.com') # first run to collect url in crawled_links
     assert crawl(test_session,url,links,'www.example.com') == None
-    captured = capsys.readouterr()
-    assert "already visited" in captured.out
+    captured = caplog.text
+    assert "already visited" in captured
 
-def test_crawl_url_out_of_domain_not_collected(test_session, capsys):
+def test_crawl_url_out_of_domain_not_collected(test_session, caplog):
+    caplog.set_level(logging.DEBUG)
     links = set()
     url='http://www.example.com/xmpl2'
     assert crawl(test_session,url,links,'www.not_correct_domain.com') == None
-    captured = capsys.readouterr()
-    assert "out of domain" in captured.out
+    captured = caplog.text
+    assert "out of domain" in captured
 
 def test_gather_links_hp(test_session):
     links = set()
